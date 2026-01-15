d9f3b6b591ea90a686dd39fd0948c1608d54129f
Maximus Spolini <maximus.spolini@crpusd.org>
2026-01-15 09:31:53 -0800

    added mac compatable version of do command

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -4,32 +4,41 @@ import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 import sys
+import platform
 
-# Modify the do_command function:
-# to use the new button as needed
 def do_command(command):
-    # Modify the do_command(command) function: 
-    # to use the text box for input to the functions
-    global command_textbox, url_entry
-
     # If url_entry is blank, use localhost IP address 
     url_val = url_entry.get()
     if (len(url_val) == 0):
-        if sys.platform.startswith('win'):
-            url_val = "::1"
-        elif sys.platform == 'darwin':
-            url_val = "127.0.0.1"
+        url_val = "127.0.0.1"
+        # ::1 does not work on Mac, likely due to firewall settings
+    
     command_textbox.delete(1.0, tk.END)
     command_textbox.insert(tk.END, command + " working....\n")
     command_textbox.update()
 
-    command_list = command.split() + [url_val]
-    #command_list = [command.split(), url_val]
-
-    with subprocess.Popen(command_list, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p: #may not work on mac
+    command_list = command + " " + url_val
+    #If running on Mac, replace commands where necessary
+    if (platform.system() == "Darwin"):
+        if (command == "tracert"):
+            command = "traceroute"
+        if (command == "ping"):
+            command = "ping -c4" # Mac otherwise pings without limit
+    
+    # NOTE: For Mac, to avoid FileNotFoundError, create list of command args. (Alternative?: add shell=true option to Popen method call)
+    command_list = (command + ' ' + url_val).split()
+    
+    """
+    The following version of the subprocess failed to capture the first line of the command output
+    (because not actually line buffering, because the PIPE is not a TTY; the stdout iteration starts too late):  
+        subprocess.Popen(commandList, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)    
+    """
+    # These lines allow for real time output in the GUI
+    with subprocess.Popen(command_list, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=0, text=True) as p:
         for line in p.stdout:
             command_textbox.insert(tk.END,line)
             command_textbox.update()
+    
 
 root = tk.Tk()
 frame = tk.Frame(root)

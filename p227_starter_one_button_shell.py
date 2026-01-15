d052088b4618af1e2e9ff70e1cc1ce409323af90
Maximus Spolini <maximus.spolini@crpusd.org>
2026-01-15 09:54:10 -0800

    made the window bigger

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -43,12 +43,12 @@ def do_command(command):
 
 root = tk.Tk()
 frame = tk.Frame(root)
-frame.pack()
-root.wm_geometry("500x300")
+frame.grid()
+root.wm_geometry("1000x600")
 root.resizable(False, False)
 # creates the frame with label for the text box
 frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
-frame_URL.pack()
+frame_URL.grid()
 
 # decorative label
 url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
@@ -59,9 +59,9 @@ url_label = tk.Label(frame_URL, text="Enter a URL of interest: ",
     cursor="heart",
     fg="mediumpurple3",
     bg="black")
-url_label.pack(side=tk.LEFT)
+url_label.grid(column=1, row= 0, columnspan= 3)
 url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
-url_entry.pack(side=tk.LEFT)
+url_entry.grid(column=3)
 
 # Save function.
 def mSave():
@@ -76,14 +76,14 @@ def mSave():
 
 
 save_btn = tk.Button(text = "Save", command= mSave)
-save_btn.pack(pady=5)
+save_btn.grid(pady=5)
 
 frame = tk.Frame(root,  bg="black") # change frame color
-frame.pack()
+frame.grid()
 
 # Adds an output box to GUI.
 command_textbox = tksc.ScrolledText(frame, height=10, width=100)
-command_textbox.pack()
+command_textbox.grid()
 
 # Makes the command button pass it's name to a function using lambda
 if sys.platform.startswith('win'):
@@ -91,7 +91,7 @@ if sys.platform.startswith('win'):
 elif sys.platform == 'darwin':
     ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", command=lambda:do_command("ping -c 4"))
 
-ping_btn.pack()
+ping_btn.grid()
 
 
 
